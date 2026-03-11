c9d1796748e20b05fe07b150a077667e4baf7d05
Nikhilll69 <nikhil16thakur@gmail.com>
2026-03-11 19:20:49 +0530

    feat: add routes for user password change, profile update, avatar, and cover image management.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,LoginUser, LogoutUser, refreshToken } from "../controllers/user.controller.js";
+import { registerUser,LoginUser, LogoutUser, refreshToken, changeCurrentPassword, getCurrentUser, updateUser, updateAvatar, updateCoverImage } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -27,7 +27,17 @@ router.route("/login").post(LoginUser);
 //SECURE ROUTES
 router.route("/logout").post(verifyJWT,LogoutUser);
 
-router.route("/refresh-token").post(refreshToken)
+router.route("/refresh-token").post(refreshToken);
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassword);
+
+router.route("/current-user").get(verifyJWT,getCurrentUser);
+
+router.route("/update-user").put(verifyJWT,updateUser);
+
+router.route("/update-avatar").put(verifyJWT,upload.single("avatar"),updateAvatar);
+
+router.route("/update-cover-image").put(verifyJWT,upload.single("coverImage"),updateCoverImage);
 
 
 
